f91cc58b96bbbc4e97efcdf3c4fa163e80696931
Gökalp Avcu <avcu219@gmail.com>
2021-02-28 14:27:28 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        //Ekle Fonksiyonu
+        public void Ekle(Musteri musteri)
+            
+        {
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad + " isimli müşteri eklenmiştir.");
+            
+
+        }
+        //Listele Fonksiyonu
+        public void Listele(Musteri[] musteri)
+        {
+            foreach (var musteriler in musteri)
+            {
+                Console.WriteLine(musteriler.Ad + " " + musteriler.Soyad + " " + musteriler.Telefon);
+
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Gökalp";
+            musteri1.Soyad = "Avcu";
+            musteri1.Telefon =  5422387416;
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Tayfun";
+            musteri2.Soyad = "Dağcı";
+            musteri2.Telefon = 5068213946;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Uygar";
+            musteri3.Soyad = "Süslü";
+            musteri3.Telefon =  5678394398;
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Listele(musteriler);
+
+
+
+
+
+        }
+    }
+}
